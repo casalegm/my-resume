7116899707819871f03809d637ecc14b94a67c64
casalegm <casalegm@gmail.com>
2017-11-22 13:39:32 -0500

    flask II assignment changes

--- manage.py
+++ manage.py
@@ -1,7 +1,27 @@
 from flask_script import Manager
-from resume import app
+from resume import app, db, Professor, Course
 
 manager = Manager(app)
+
+@manager.command
+def deploy():
+    db.drop_all()
+    db.create_all()
+    White = Professor(name='White', department='MISY')
+    Gillespie = Professor(name='Gillespie', department='ACCT')
+    Wang = Professor(name='Wang', department='MISY')
+    MISY430 = Course(title='Systems Analysis and Implementation', number=430, description='Covers the challenges of developing and managing systems analysis and design projects.', professor=White)
+    ACCT425 = Course(title='Strategic Information Systems and Accounting', number=425, description='Explores the role of accounting and information systems in accomplishing the strategic goals of the corporation.', professor=White)
+    ACCT327 = Course(title='Cost Accounting', number=327, description='Process, job order and standard costing; variable and absorption costing; budgeting, decentralization, and transfer pricing; and cost analysis for managerial applications.', professor=Gillespie)
+    MISY350 = Course(title='Business Application Development II', number=350, description='Covers concepts related to client side development, including cascading style sheets and JavaScript.', professor=Wang)
+    db.session.add(White)
+    db.session.add(Gillespie)
+    db.session.add(Wang)
+    db.session.add(MISY430)
+    db.session.add(ACCT425)
+    db.session.add(ACCT327)
+    db.session.add(MISY350)
+    db.session.commit()
 
 if __name__ == '__main__':
     manager.run()
--- resume.py
+++ resume.py
@@ -1,22 +1,127 @@
-from flask import Flask, render_template
+import os
+from flask import Flask, session, render_template, request, flash, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
+
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'hard to guess secure key'
+
+# setup SQLAlchemy
+basedir = os.path.abspath(os.path.dirname(__file__))
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'data.sqlite')
+db = SQLAlchemy(app)
+
+# define database tables
+class Professor(db.Model):
+    __tablename__ = 'professors'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64))
+    department = db.Column(db.Text)
+    courses = db.relationship('Course', backref='professor')
+
+
+class Course(db.Model):
+    __tablename__ = 'courses'
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(256))
+    number = db.Column(db.Integer)
+    description = db.Column(db.Text)
+    professor_id = db.Column(db.Integer, db.ForeignKey('professors.id'))
 
 
 @app.route('/')
 def home():
     return render_template('home.html')
 
+@app.route('/professors')
+def show_all_professors():
+    professors = Professor.query.all()
+    return render_template('professor-all.html', professors=professors)
+
+
+@app.route('/professor/add', methods=['GET', 'POST'])
+def add_professors():
+    if request.method == 'GET':
+        return render_template('professor-add.html')
+    if request.method == 'POST':
+        # get data from the form
+        name = request.form['name']
+        department = request.form['department']
+
+        # insert the data into the database
+        professor = Professor(name=name, department=department)
+        db.session.add(professor)
+        db.session.commit()
+        return redirect(url_for('show_all_professors'))
+
+
+@app.route('/professor/edit/<int:id>', methods=['GET', 'POST'])
+def edit_professor(id):
+    professor = Professor.query.filter_by(id=id).first()
+    if request.method == 'GET':
+        return render_template('professor-edit.html', professor=professor)
+    if request.method == 'POST':
+        # update data based on the form data
+        professor.name = request.form['name']
+        professor.department = request.form['department']
+        # update the database
+        db.session.commit()
+        return redirect(url_for('show_all_professors'))
+
+@app.route('/courses')
+def show_all_courses():
+    courses = Course.query.all()
+    return render_template('course-all.html', courses=courses)
+
+
+@app.route('/course/add', methods=['GET', 'POST'])
+def add_courses():
+    if request.method == 'GET':
+        professors = Professor.query.all()
+        return render_template('course-add.html', professors=professors)
+    if request.method == 'POST':
+        # get data from the form
+        title = request.form['title']
+        number = request.form['number']
+        description = request.form['description']
+        professor_name = request.form['professor']
+        professor = Professor.query.filter_by(name=professor_name).first()
+        course = Course(title=title, number=number, description=description, professor=professor)
+
+        # insert the data into the database
+        db.session.add(course)
+        db.session.commit()
+        return redirect(url_for('show_all_courses'))
+
+
+@app.route('/course/edit/<int:id>', methods=['GET', 'POST'])
+def edit_course(id):
+    course = Course.query.filter_by(id=id).first()
+    professors = Professor.query.all()
+    if request.method == 'GET':
+        return render_template('course-edit.html', course=course, professors=professors)
+    if request.method == 'POST':
+        # update data based on the form data
+        course.title = request.form['title']
+        course.number = request.form['number']
+        course.description = request.form['description']
+        professor_name = request.form['professor']
+        professor = Professor.query.filter_by(name=professor_name).first()
+        course.professor = professor
+        # update the database
+        db.session.commit()
+        return redirect(url_for('show_all_courses'))
+
 @app.route('/about')
 def about():
     return render_template('about.html')
 
-@app.route('/courses')
-def get_all_courses():
-    courses = [
-        'MISY225 - Introduction to Programming Business Applications',
-        'MISY350 - Business Application Development II',
-        'MISY430 - Systems Analysis and Implementation']
-    return render_template('courses.html', courses=courses)
+#@app.route('/taken')
+#def get_all_courses():
+    #courses = [
+        #'MISY225 - Introduction to Programming Business Applications',
+        #'MISY350 - Business Application Development II',
+        #'MISY430 - Systems Analysis and Implementation']
+    #return render_template('courses.html', courses=courses)
 
 
 if __name__ == '__main__':
